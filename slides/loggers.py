604ae8f00f83eba49dfc85c395057393d5c981fd
Jarvys <yuhan534@126.com>
2014-11-03 18:15:59 +0800

    first commit

---
+++ slides/loggers.py
@@ -0,0 +1,56 @@
+import os
+
+BASE_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'logs')
+
+LOGGING = {
+    'version': 1,
+    'dusable_existing_loggers': True,
+    'formatters': {
+        'normal': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(module)s %(message)s'
+            }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console':{
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'django': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'when': 'D',
+            'interval': 1,
+            'backupCount': 5,
+            'filename': os.path.join(BASE_DIR, 'django.log'),
+            'formatter': 'normal'
+        },
+        'app': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'when': 'D',
+            'interval': 1,
+            'backupCount': 5,
+            'filename': os.path.join(BASE_DIR, 'app.log'),
+            'formatter': 'normal'
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['django'],
+            'propagate': False,
+            'level': 'DEBUG'
+        },
+        '': {
+            'handlers': ['app'],
+            'level': 'DEBUG'
+        }
+    }
+}
